02e1c99c5ceb1780698c4f91df60be9a04d3cc92
Berend92 <berend.mul@student.uva.nl>
2014-10-01 18:50:23 +0200

    Create NEW VERSION grep_AC-FIELD_write_to_CSVfile.sh
    The new version with all the changes we talked about on wednesday.

---
+++ NEW VERSION grep_AC-FIELD_write_to_CSVfile.sh
@@ -0,0 +1,54 @@
+# grep_AC-FIELD_write_to_CSVfile.sh
+# a lot of files still have to be filtered (grep and grep -v)
+
+FILE=$1
+
+grep -i -w 'liberal arts' $FILE > arts
+
+grep -i -w 'art histor*' $FILE > art_history1.csv
+grep -i -w 'archaeology and art' $FILE > art_history2.csv
+grep -i -w 'archeology and art' $FILE > art_history3.csv
+grep -i -w 'architecture' $FILE > art_history4.csv
+
+grep -i    'classic*' $FILE > classics1.csv
+grep -i -w 'Latin' $FILE > classics2.csv
+grep -i -w 'Ancient Greek' $FILE > classics3.csv
+
+grep -i    'histor*' $FILE > histor1.csv
+
+grep -i -w 'film studies' $FILE > film_studies1.csv
+grep -i    'cinema' $FILE > film_studies2.csv
+
+grep -i -w 'language' $FILE > language1.csv
+grep -i    'linguistic*' $FILE > language2.csv
+grep -i -w 'grammar' $FILE > language3.csv
+
+grep -i -w 'literature' $FILE > literature1.csv
+grep -i -w 'book studies' $FILE > literature2.csv
+grep -i -w 'literary' $FILE > literature3.csv
+grep -i -w 'poetry' $FILE > literature4.csv
+grep -i -w 'prose' $FILE > literature5.csv
+
+grep -i -w 'music' $FILE > music1.csv
+grep -i -w 'musicology' $FILE > music2.csv
+grep -i -w 'musical' $FILE > music3.csv
+grep -i -w 'acoustics' $FILE > music4.csv
+
+grep -i -w 'performing arts' $FILE > performing_arts1.csv
+grep -i -w 'visual arts' $FILE > performing_arts2.csv
+grep -i    'theat*' $FILE > performing_arts3.csv
+grep -i -w 'photography' $FILE > performing_arts4.csv
+grep -i    'musician*' $FILE > performing_arts5.csv
+grep -i -w 'dramatics' $FILE > performing_arts6.csv
+grep -i -w 'sculpting' $FILE > performing_arts7.csv
+grep -i -w 'painting' $FILE > performing_arts8.csv
+
+grep -i    'philosoph*' $FILE > philosophy1.csv
+grep -i -w 'ethics' $FILE > philosophy2.csv
+grep -i -w 'epistemology' $FILE > philosophy3.csv
+grep -i -w 'metaphysics' $FILE > philosophy4.csv
+
+grep -i -w 'religion' $FILE > religion1.csv
+grep -i    'religious*' $FILE > religion2.csv
+grep -i    'theolog*' $FILE > religion3.csv
+
