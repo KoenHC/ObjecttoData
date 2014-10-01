4f444c8f5d4374c06500c85a37e6f5024a2102f3
ArletteMauritsz <arlettemauritsz@outlook.com>
2014-10-01 19:26:38 +0200

    Update grep_AC-FIELD_write_to_JSONfile.sh

--- grep_AC-FIELD_write_to_JSONfile.sh
+++ grep_AC-FIELD_write_to_JSONfile.sh
@@ -4,31 +4,25 @@ FILE=$1
 
 grep -i -w 'liberal arts' $FILE > arts.json
 
-ART HISTORY
 grep -i 'art histor*' $FILE > art_history1.json
 grep -i -w 'art and archaeology' $FILE > art_history2.json
 grep -i -w 'architecture' $FILE > art_history3.json
 
-CLASSICS
-grep -i    'classic*' $FILE > classics1.json
+grep -i 'classic*' $FILE > classics1.json
 grep -i -w 'Latin' $FILE > classics2.json
 grep -v -i America* classics2.json
 grep -i -w 'ancient Greek' $FILE > classics3.json
 
-HISTORY
 grep -i -w 'histor*' $FILE > history.json
 grep -v -i -w 'art' history.json
 
-FILM STUDIES
 grep -i -w 'film studies' $FILE > film_studies1.json
-grep -i    'cinema' $FILE > film_studies2.json
+grep -i 'cinema' $FILE > film_studies2.json
 
-LANGUAGE
 grep -i 'language*' $FILE > language1.json
 grep -i 'linguistic*' $FILE > language2.json
 grep -i -w 'grammar' $FILE > language3.json
 
-LITERATURE
 grep -i -w 'literature' $FILE > literature1.json
 grep -i -w 'comparative literature' $FILE > literature2.json
 grep -i -w 'book studies' $FILE > literature3.json
@@ -36,13 +30,11 @@ grep -i -w 'literary' $FILE > literature4.json
 grep -i -w 'poetry' $FILE > literature5.json
 grep -i -w 'prose' $FILE > literature6.json
 
-MUSIC
 grep -i -w 'music' $FILE > music1.json
 grep -i -w 'musicology' $FILE > music2.json
 grep -i -w 'musical' $FILE > music3.json
 grep -i -w 'acoustics' $FILE > music4.json
 
-PERFORMING ARTS
 grep -i -w 'performing arts' $FILE > peforming_arts1.json
 grep -i -w 'visual arts' $FILE > peforming_arts2.json
 grep -i 'theat*' $FILE > peforming_arts3.json
@@ -53,13 +45,11 @@ grep -i -w 'dramatics' $FILE > peforming_arts6.json
 grep -i -w 'sculpting' $FILE > peforming_arts7.json
 grep -i -w 'painting' $FILE > peforming_arts8.json
 
-PHILOSOPHY
 grep -i -w 'philosoph*' $FILE > philosophy1.json
 grep -i -w 'ethics' $FILE > philosophy2.json
 grep -i -w 'epistemology' $FILE > philosophy3.json
 grep -i -w 'metaphysics' $FILE > philosophy4.json
 
-RELIGION
 grep -i -w 'religion' $FILE > religion1.json
 grep -i 'religious*' $FILE > religion2.json
 grep -i 'theolog*' $FILE > religion3.json
