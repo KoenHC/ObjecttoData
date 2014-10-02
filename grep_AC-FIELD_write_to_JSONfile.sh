98560b78701a54ad1199db233256ed09715aa37f
KoenHC <k.vanschaik@quicknet.nl>
2014-10-02 10:34:54 +0200

    Update grep_AC-FIELD_write_to_JSONfile.sh

--- grep_AC-FIELD_write_to_JSONfile.sh
+++ grep_AC-FIELD_write_to_JSONfile.sh
@@ -13,11 +13,11 @@ grep -i -w 'Latin' $FILE > classics2.json
 grep -v -i America* classics2.json
 grep -i -w 'ancient Greek' $FILE > classics3.json
 
-grep -i -w 'histor*' $FILE > history.json
+grep -i 'histor*' $FILE > history.json
 grep -v -i -w 'art' history.json
 
 grep -i -w 'film studies' $FILE > film_studies1.json
-grep -i 'cinema' $FILE > film_studies2.json
+grep -i -w 'cinema' $FILE > film_studies2.json
 
 grep -i 'language*' $FILE > language1.json
 grep -i 'linguistic*' $FILE > language2.json
