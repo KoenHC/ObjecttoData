a95639949d7b4309b6ab66c317594bbb024e342e
ArletteMauritsz <arlettemauritsz@outlook.com>
2014-10-09 21:09:43 +0200

    Update grep_AC-FIELD_write_to_CSVfile.sh

--- grep_AC-FIELD_write_to_CSVfile.sh
+++ grep_AC-FIELD_write_to_CSVfile.sh
@@ -1,12 +1,11 @@
 # grep_AC-FIELD_write_to_CSVfile.sh
 
 
-grep -i -w 'liberal arts' $FILE > arts.csv
+grep -i -w 'liberal arts' $FILE > liberal_arts.csv
 
 grep -i 'art histor*' $FILE > art_history1.csv
 grep -i -w 'art and archaeology' $FILE > art_history2.csv
-grep -i -w 'architecture' $FILE > art_history3.csv
-grep -i 'history of art*' $FILE > art_history4.csv
+grep -i 'history of art*' $FILE > art_history3.csv
 
 grep -i -w 'classics' $FILE > classics1.csv
 grep -i -w 'Latin' $FILE > classics_temp.csv
@@ -23,12 +22,14 @@ grep -i -w 'Middle Ages' $FILE > history3.csv
 grep -i -w 'Early Modern' $FILE > history4.csv
 rm history_temp.csv
 
-grep -i -w 'film' $FILE > film_studies1.csv
-grep -i -w 'cinema' $FILE > film_studies2.csv
-
 grep -i 'language*' $FILE > language1.csv
 grep -i 'linguistic*' $FILE > language2.csv
 grep -i -w 'grammar' $FILE > language3.csv
+
+grep -i -w 'film studies' $FILE > media_studies1.csv
+grep -i -w 'cinema studies' $FILE > media_studies2.csv
+grep -i -w 'musicology' $FILE > media_studies3.csv
+grep -i -w 'theater studies' $FILE > media_studies4.csv
 
 grep -i -w 'literature' $FILE > literature1.csv
 grep -i -w 'book studies' $FILE > literature2.csv
@@ -36,21 +37,23 @@ grep -i -w 'literary' $FILE > literature3.csv
 grep -i -w 'poetry' $FILE > literature4.csv
 grep -i -w 'prose' $FILE > literature5.csv
 
-grep -i -w 'music' $FILE > music1.csv
-grep -i -w 'musicology' $FILE > music2.csv
-grep -i -w 'musical' $FILE > music3.csv
-grep -i -w 'acoustics' $FILE > music4.csv
-
-grep -i -w 'performing arts' $FILE > performing_arts1.csv
-grep -i -w 'visual arts' $FILE > performing_arts2.csv
-grep -i 'theat*' $FILE > performing_arts_temp.csv
-grep -v -i 'Southeast*' performing_arts_temp.csv > performing_arts3.csv
-grep -i -w 'photography' $FILE > performing_arts4.csv
-grep -i -w 'musician' $FILE > performing_arts5.csv
-grep -i -w 'dramatics' $FILE > performing_arts6.csv
-grep -i -w 'sculpting' $FILE > performing_arts7.csv
-grep -i -w 'painting' $FILE > performing_arts8.csv
-rm performing_arts_temp.csv
+grep -i -w 'visual arts' $FILE > arts1.csv
+grep -i -w 'architecture' $FILE > arts2.csv
+grep -i -w 'painting' $FILE > arts3.csv
+grep -i -w 'drawing' $FILE > arts4.csv
+grep -i -w 'photography' $FILE > arts5.csv
+grep -i -w 'performing arts' $FILE > arts6.csv
+grep -i -w 'music' $FILE > arts7.csv
+grep -i -w 'film' $FILE > arts_temp.csv
+grep -i -w -v 'film studies' arts_temp.csv > arts8.csv
+grep -i 'theat*' $FILE > arts_temp2.csv
+grep -i -v 'Southeast*' arts_temp2.csv > arts_temp3.csv
+grep -i -w -v 'theater studies' arts_temp3.csv > arts9.csv
+grep -i -w 'dance' $FILE > arts10.csv
+grep -i -w 'dramatics' $FILE > arts11.csv
+rm arts_temp.csv
+rm arts_temp2.csv
+rm arts_temp3.csv
 
 grep -i 'philosoph*' $FILE > philosophy1.csv
 grep -i -w 'ethics' $FILE > philosophy2.csv
